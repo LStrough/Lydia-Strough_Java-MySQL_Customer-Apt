9dba51c0a521e8d4619cfad6b58bcf0c9ddfa815
Lydia Strough <lstroug@wgu.edu>
2022-06-11 15:44:57 -0700

    CustomerDaoImpl methods pass! (except delete)

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,14 +26,22 @@ public class Main extends Application {
         JDBC.closeConnection();
          */
 
+        /*
         JDBC.openConnection();
         CustomerDao customerDao = new CustomerDaoImpl();
         DivisionDao divisionDao = new DivisionDaoImpl();
         //System.out.println(customerDao.getAllCustomers());
         //System.out.println(divisionDao.getDivisionsByCountry(1));
-        //System.out.println(divisionDao.getDivision()); //WA
+        //System.out.println(divisionDao.getDivision(46)); //WA
         //System.out.println(customerDao.getCustomer(1));
-        //customerDao.addCustomer("Lydia", "12228 56th Drive NE", "98271", "867-5309", ""); //washington division id
-        //System.out.println(customerDao.getCustomer());
+        //customerDao.addCustomer("Lydia", "12228 56th Drive NE", "98271", "867-5309", 46); //washington division id
+        //System.out.println(customerDao.getCustomer(4));
+        //customerDao.updateCustomerName(4, "Lydia", "Lydia Strough");
+        //customerDao.updateCustomerAddress(4, "2400 N Arivona Ave", "2400 N Arizona Ave");
+        //customerDao.updateCustomerPostalCode(4, "98271", "85225");
+        //customerDao.updateCustomerDivision(4, 46, 2);
+        //customerDao.updateCustomerPhone(4, "867-5309", "310-0771");
+        customerDao.deleteCustomer(4, "Lydia Strough");
+         */
     }
 }
