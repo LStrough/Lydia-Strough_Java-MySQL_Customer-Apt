d58db2fe0c7e27b199c2b488b82abfa4c1d9addf
Lydia Strough <strough.lydia@gmail.com>
2022-06-11 15:28:55 -0700

    test CustomerDaoImpl() in main

--- src/DAO/CustomerDao.java
+++ src/DAO/CustomerDao.java
@@ -6,9 +6,13 @@ import model.Customer;
 public interface CustomerDao {
     public ObservableList<Customer> getAllCustomers();
     public Customer getCustomer(int customerId);
-    public void updateCustomer(int index, Customer newCustomer);
-    public boolean deleteCustomer(Customer selectedCustomer);
-    public void addCustomer(Customer customer);
+    public int updateCustomerName(int customerId, String currentName, String newName);
+    public int updateCustomerPhone(int customerId, String currentPhone, String newPhone);
+    public int updateCustomerAddress(int customerId, String currentAddress, String newAddress);
+    public int updateCustomerPostalCode(int customerId, String currentPostalCode, String newPostalCode);
+    public int updateCustomerDivision(int customerId, int currentDivisionId, int newDivisionId);
+    public int deleteCustomer(int customerId, String customerName);
+    public int addCustomer(String customerName, String address, String postalCode, String phone, int divisionId);
     public Customer lookUpCustomer(int customerId);
     public ObservableList<Customer>lookUpCustomer(String customerName);
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Division;
 
 public class Main extends Application {
     @Override
@@ -19,8 +20,20 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         //Locale.setDefault(new Locale("fr"));
+        /*
         JDBC.openConnection();
         launch(args);
         JDBC.closeConnection();
+         */
+
+        JDBC.openConnection();
+        CustomerDao customerDao = new CustomerDaoImpl();
+        DivisionDao divisionDao = new DivisionDaoImpl();
+        //System.out.println(customerDao.getAllCustomers());
+        //System.out.println(divisionDao.getDivisionsByCountry(1));
+        //System.out.println(divisionDao.getDivision()); //WA
+        //System.out.println(customerDao.getCustomer(1));
+        //customerDao.addCustomer("Lydia", "12228 56th Drive NE", "98271", "867-5309", ""); //washington division id
+        //System.out.println(customerDao.getCustomer());
     }
 }
