903226da6794a61bd61001584b87691f855d8bc5
Lydia Strough <lstroug@wgu.edu>
2022-06-25 15:46:06 -0400

    javadoc Main Customers, started.

--- src/controller/MainCustomers.java
+++ src/controller/MainCustomers.java
@@ -20,15 +20,61 @@ import java.time.ZoneId;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
+/**
+ * This is the "Main Customers" Menu controller.
+ *
+ * <p>This page displays a current list of the databases' customers, and gives the user the ability to search customers
+ * by customer ID and customer name. The user also has the ability to add, modify (update), and delete customers
+ * as well as their associated appointments, from the database. This page also offers the user the ability to navigate the
+ * rest of the program. The user can also exit the program from this page.</p>
+ *
+ * @author Lydia Strough
+ */
 public class MainCustomers implements Initializable {
-    private Stage stage;
-    private Parent scene;
+    Stage stage;
+    Parent scene;
 
+    /**
+     * This is the "search customer by customer ID or customer name" text field
+     */
     public TextField searchCustomer;
+    /**
+     * This is the customer table
+     */
     public TableView<Customer> customerTableView;
-    public TableColumn customerIdCol, customerNameCol, addressCol, postalCodeCol, phoneNumCol, stateProvinceCol;
+    /**
+     * This is the customer ID column
+     */
+    public TableColumn customerIdCol;
+    /**
+     * This is the customer name column
+     */
+    public TableColumn customerNameCol;
+    /**
+     * This is the customer address column
+     */
+    public TableColumn addressCol;
+    /**
+     * This is the customer postal code column
+     */
+    public TableColumn postalCodeCol;
+    /**
+     * This is the customer phone number column
+     */
+    public TableColumn phoneNumCol;
+    /**
+     * This is the customer division column
+     */
+    public TableColumn stateProvinceCol;
+    /**
+     * This is the (system default) time zone label
+     */
     public Label userTimeZoneLbl;
 
+    /**
+     *
+     * @param actionEvent
+     */
     public void onActionSearchCustomer(ActionEvent actionEvent) {
         JDBC.openConnection();
         CustomerDao customerDao = new CustomerDaoImpl();
@@ -52,6 +98,14 @@ public class MainCustomers implements Initializable {
         }
     }
 
+    /**
+     * This is the "Add Customer" method.
+     *
+     * <p>The scene shifts from the Main Customers Menu screen, to the Add Customer form. </p>
+     *
+     * @param actionEvent the add customer button is clicked
+     * @throws IOException
+     */
     public void onActionAddCustomer(ActionEvent actionEvent) throws IOException {
         System.out.println("Add Customer Button Clicked!");
 
@@ -61,6 +115,18 @@ public class MainCustomers implements Initializable {
         stage.show();
     }
 
+    /**
+     * This is the "Update Customer" method.
+     *
+     * <p>The scene is shifted from the Main Customers Menu to the Update Customer form.</p>
+     *
+     * <p>The user is expected to select a customer from the customerTableView. If the user fails to do so,
+     * a runtime exception is created, and an alert dialogue box populates with the following error message:
+     * "Please select a customer to update!".</p>
+     *
+     * @param actionEvent the update customer button is clicked
+     * @throws IOException
+     */
     public void onActionUpdateCustomer(ActionEvent actionEvent) throws IOException {
         System.out.println("Update Customer Button Clicked!");
 
@@ -86,6 +152,10 @@ public class MainCustomers implements Initializable {
         }
     }
 
+    /**
+     *
+     * @param actionEvent
+     */
     public void onActionDeleteCustomer(ActionEvent actionEvent) {
         System.out.println("Delete Customer Button Clicked!");
 
@@ -108,6 +178,14 @@ public class MainCustomers implements Initializable {
         }
     }
 
+    /**
+     * This is the "Appointments" method.
+     *
+     * <p>The scene shifts from the Main Customers Menu screen, to the Main Appointments Menu. </p>
+     *
+     * @param actionEvent the appointments button is clicked
+     * @throws IOException
+     */
     public void onActionAppointments(ActionEvent actionEvent) throws IOException {
         System.out.println("Appointments Button Clicked!");
 
@@ -117,6 +195,14 @@ public class MainCustomers implements Initializable {
         stage.show();
     }
 
+    /**
+     * This is the "Reports" method.
+     *
+     * <p>The scene shifts from the Main Customers Menu screen, to the Contact Report screen.</p>
+     *
+     * @param actionEvent the reports button is clicked
+     * @throws IOException
+     */
     public void onActionReports(ActionEvent actionEvent) throws IOException {
         System.out.println("Reports Button Clicked!");
 
@@ -126,6 +212,15 @@ public class MainCustomers implements Initializable {
         stage.show();
     }
 
+    /**
+     * This is the "Logout" method.
+     *
+     * <p>A confirmation dialog box populates: "Do you wish to Exit the program?".
+     * If the user hits the OK button, the program ends. If cancel is selected, the user
+     * stays in the Main Customers Menu.</p>
+     *
+     * @param actionEvent the logout button is clicked
+     */
     public void onActionLogout(ActionEvent actionEvent) {
         System.out.println("Logout Button Clicked!");
 
@@ -140,6 +235,9 @@ public class MainCustomers implements Initializable {
         }
     }
 
+    /**
+     *
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Customer Records: I am initialized!");
